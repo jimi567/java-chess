756c471b0a8ce7c07a4a2ebdeea46ac823893a37
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-03-29 22:51:17 +0900

    feat: 실행 상태 중 흰팀 턴인 경우를 구현

---
+++ src/main/java/chess/domain/game/WhiteTurn.java
@@ -0,0 +1,40 @@
+package chess.domain.game;
+
+import chess.domain.board.Board;
+import chess.domain.board.Coordinate;
+import chess.domain.piece.Piece;
+import chess.domain.piece.Team;
+import chess.domain.piece.fixedmove.King;
+import java.util.NoSuchElementException;
+
+public class WhiteTurn extends Running {
+    protected WhiteTurn(final Board board) {
+        super(board);
+    }
+
+    public State move(final Coordinate source, final Coordinate target) {
+        validateCoordinate(source, target);
+        validateSourcePiece(board.findByCoordinate(source));
+        Piece captured = board.move(source, target);
+        if (captured instanceof King) {
+            return new End();
+        }
+        return new BlackTurn(board);
+    }
+
+    private void validateCoordinate(final Coordinate source, final Coordinate target) {
+        if (source.equals(target)) {
+            throw new IllegalArgumentException("동일한 위치로 이동할 수 없습니다.");
+        }
+    }
+
+    private void validateSourcePiece(final Piece sourcePiece) {
+        if (sourcePiece.isEmpty()) {
+            throw new NoSuchElementException("보드에 움직일 대상 기물이 없습니다.");
+        }
+
+        if (sourcePiece.isNotSameTeam(Team.WHITE)) {
+            throw new IllegalStateException("지금은 흰팀의 차례입니다.");
+        }
+    }
+}
