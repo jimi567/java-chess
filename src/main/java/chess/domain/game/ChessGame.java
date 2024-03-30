092570374d2be1c1bd7b887da022a912934da8f2
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-03-30 21:14:36 +0900

    refactor: 체스 게임 추가하여, State와 board 분리

--- src/main/java/chess/domain/game/ChessGame.java
+++ src/main/java/chess/domain/game/ChessGame.java
@@ -1,16 +1,48 @@
 package chess.domain.game;
 
 import chess.domain.board.Board;
+import chess.domain.board.Coordinate;
+import chess.domain.game.state.Ready;
+import chess.domain.game.state.State;
+import chess.domain.piece.Team;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
-public abstract class ChessGame implements State {
+public class ChessGame {
+    private final Board board;
+    private State state;
 
-    protected final Board board;
 
-    protected ChessGame(final Board board) {
-        this.board = board;
+    public ChessGame() {
+        this.board = new Board();
+        state = new Ready();
     }
 
-    @Override
+    public void start() {
+        state = state.start();
+    }
+
+    public void move(Coordinate source, Coordinate target) {
+        state = state.move(board, source, target);
+    }
+
+    public ChessStatus status() {
+        state = state.status();
+        Map<Team, BigDecimal> scores = new HashMap<>();
+        scores.put(Team.BLACK, board.totalScoreByTeam(Team.BLACK));
+        scores.put(Team.WHITE, board.totalScoreByTeam(Team.WHITE));
+        return new ChessStatus(scores);
+    }
+
+    public void end() {
+        state = state.end();
+    }
+
+    public boolean isRunning() {
+        return state.isRunning();
+    }
+
     public Board board() {
         return board;
     }
