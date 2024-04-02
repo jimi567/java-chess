97fc0f39f26c130d001db24e1d9240e95bb4ae32
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-04-02 16:41:34 +0900

    fix: Connection 정보 수정

--- src/main/java/chess/db/DBConnection.java
+++ src/main/java/chess/db/DBConnection.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String SERVER = "localhost:13306"; // MySQL 서버 주소
     private static final String DATABASE = "chess"; // MySQL DATABASE 이름
-    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USERNAME = "root"; //  MySQL 서버 아이디
-    private static final String PASSWORD = "root"; // MySQL 서버 비밀번호
+    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Seoul";
+    private static final String USERNAME = "user"; //  MySQL 서버 아이디
+    private static final String PASSWORD = "password"; // MySQL 서버 비밀번호
 
     private DBConnection() {
     }
