5733006dc5290260ac4b80d4e1869b8608fdcafc
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-04-03 14:56:10 +0900

    feat: chess_game, piece DDL 작성

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,10 +1,33 @@
 USE chess;
 
+CREATE TABLE chess_game
+(
+    id    BIGINT      NOT NULL AUTO_INCREMENT,
+    name  VARCHAR(32) NOT NULL,
+    state VARCHAR(8)  NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE movement
 (
-    movement_id       INT        NOT NULL AUTO_INCREMENT,
+    movement_id       BIGINT     NOT NULL AUTO_INCREMENT,
+    chess_game_id     BIGINT     NOT NULL,
     source_coordinate VARCHAR(2) NOT NULL,
     target_coordinate VARCHAR(2) NOT NULL,
 
-    PRIMARY KEY (movement_id)
+    PRIMARY KEY (movement_id),
+    FOREIGN KEY (chess_game_id) REFERENCES chess_game (id)
+);
+
+CREATE TABLE piece
+(
+    piece_id      BIGINT     NOT NULL AUTO_INCREMENT,
+    chess_game_id BIGINT     NOT NULL,
+    coordinate    VARCHAR(2) NOT NULL,
+    team          VARCHAR(8) NOT NULL,
+    type          VARCHAR(8) NOT NULL,
+
+    PRIMARY KEY (piece_id),
+    FOREIGN KEY (chess_game_id) REFERENCES chess_game (id)
 );
